f6ae417bd29f08c8ef7d6e47812c1cd00fbf0bd3
AvasBrahma <avasbrahma@gmail.com>
2023-07-13 20:35:42 +0530

    Express setup and Folder Directory Setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express=require('express');
+
+
+const app=express();
+
+
+
+// Start the server
+const port=8000;
+app.listen(port, function(err){
+
+    if(err){
+        console.log(`Error running in server : ${err}`);
+    }
+    console.log(`Server is running in port : ${port}`);
+});
