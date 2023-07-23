f5347c0c29e944ac5deab69e275343f42d03aca1
AvasBrahma <avasbrahma@gmail.com>
2023-07-23 21:36:22 +0530

    User schema with login funtionality implemented without auth

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,6 +1,123 @@
-
-module.exports.registerUser=function(req, res){
-    return res.status(200).send({ 
-        message: 'User Registered.....',
+const User = require("../models/users_Schema");
+const bcrypt = require("bcryptjs");
+module.exports.registerUser = async function (req, res) {
+  try {
+    let user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      passwordHash: bcrypt.hashSync(req.body.password, 9),
+      phone: req.body.phone,
+      isAdmin: req.body.isAdmin,
+      street: req.body.street,
+      appartments: req.body.appartments,
+      zip: req.body.zip,
+      city: req.body.city,
+      country: req.body.country,
     });
+
+    user = await user.save();
+    if (user) {
+      return res.status(200).send({
+        message: "New User Registered",
+        user,
+      });
+    } else {
+      return res.status(404).send("Cannot Register User");
+    }
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      error,
+    });
+  }
+};
+
+//function to get all the User
+module.exports.getAllUser = async function (req, res) {
+  try {
+    const userList = await User.find().select('-passwordHash');
+
+    if (userList) {
+      res.status(200).json({
+        success: true,
+        message: "List of All User :",
+        userList,
+      });
+    } else {
+      res.status(500).json({
+        success: false,
+        message: "No User Found",
+      });
+    }
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      error,
+    });
+  }
+};
+
+//function to get User by Id
+module.exports.getUserById = async function (req, res) {
+  try {
+    const user = await User.findById(req.params.id).select('-passwordHash');
+
+    if (user) {
+      res.status(200).json({
+        success: true,
+        message: "Requested User :",
+        user,
+      });
+    } else {
+      res.status(500).json({
+        success: false,
+        message: "No User found with the requested Id",
+      });
+    }
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      error,
+    });
+  }
+};
+
+//function to login
+module.exports.login=async function (req, res) {
+
+    try {
+        const user=await User.findOne({
+            email:req.body.email
+        })
+        if(user){
+
+            if(user && bcrypt.compareSync(req.body.password, user.passwordHash)){
+                res.status(200).json({
+                    success: true,
+                    message: "User Authenticated",
+                    user,
+                  });
+            }else{
+                res.status(200).json({
+                    success: false,
+                    message: "Password is Incorrect"
+                  });
+            }
+          
+        }else{
+            res.status(400).json({
+                success: false,
+                message: "User Not Found"
+              });
+        }
+        
+    } catch (error) {
+        return res.status(400).json({
+            success: false,
+            error,
+          });
+    }
+
+    
+
 }
--- routes/user/index.js
+++ routes/user/index.js
@@ -3,8 +3,12 @@ const router=express.Router();
 
 const authContoller=require('../../controllers/auth_controller');
 
-router.get('/register', authContoller.registerUser);
+router.post('/register', authContoller.registerUser);
 
+router.get('/alluser', authContoller.getAllUser);
 
+router.get('/:id', authContoller.getUserById);
+
+router.post('/login', authContoller.login);
 
 module.exports=router;
