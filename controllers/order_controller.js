b7bc13cd8cf93f24edc818af20a1c2e28e60a9ff
AvasBrahma <avasbrahma@gmail.com>
2023-07-25 01:27:49 +0530

    Implementde to calculate total price for orders

--- controllers/order_controller.js
+++ controllers/order_controller.js
@@ -15,12 +15,33 @@ module.exports.getAllOrder=async function(req, res){
     }else{
         res.status(500).json({
             success: false,
-            message: 'No Category'
+            message: 'No Order Found'
         })
     }
 
 }
 
+//function to get one order details
+module.exports.getOrderById=async function(req, res){
+    const orderDetail= await Order.findById(req.params.id).populate('user', 'name')
+    .populate({path: 'orderItems', populate: {
+             path: 'product' , populate: 'category'
+    }});
+
+    if(orderDetail){
+        res.status(200).json({
+            success:true,
+            message: 'Order Detail :',
+            orderDetail
+        })
+    }else{
+        res.status(500).json({
+            success: false,
+            message: 'No Order Found'
+        })
+    }
+
+}
 
 //function to post Orders 
 module.exports.postOrders=async function(req, res){
@@ -36,7 +57,16 @@ module.exports.postOrders=async function(req, res){
      }))
 
      const orderItemIdsResolved=await orderItemsIds;
+      
+     //calculating total price:
+     const totalPrices=await Promise.all(orderItemIdsResolved.map(async orderItemId=>{
+         const orderItem=await OrderItem.findById(orderItemId).populate('product', 'price');
+         const totalPrice=orderItem.product.price * orderItem.quantity;
+         return totalPrice;
+        }))
 
+    console.log("Total Prices : ", totalPrices);
+    const totalPrice=totalPrices.reduce((a,b)=>a+b,0);
     let order= new Order({
         orderItems: orderItemIdsResolved,
         shippingAddress1: req.body.shippingAddress1,
@@ -46,7 +76,7 @@ module.exports.postOrders=async function(req, res){
         country: req.body.country,
         phone: req.body.phone,
         status: req.body.status,
-        totalPrice:req.body.totalPrice,
+        totalPrice: totalPrice,
         user: req.body.user
     })
     
@@ -61,4 +91,54 @@ module.exports.postOrders=async function(req, res){
     }
    
   
+}
+
+
+//function to update status only in Orders 
+module.exports.updateOrderStatus=async function(req, res){
+    const order= await Order.findByIdAndUpdate(req.params.id, {
+      status: req.body.status
+    },{
+       new: true
+    })
+
+   if(order){
+       res.status(200).json({
+           success:true,
+           message: 'Order Status Updated',
+           order
+       })
+   }else{
+       res.status(500).json({
+           success: false,
+           message: 'Cannot Update the Order Status'
+       })
+   }
+}
+
+// function to delete the Order with id
+module.exports.deleteOrder= function(req, res){
+
+    Order.findByIdAndRemove(req.params.id).then(async order=>{
+        if(order){
+            // looping the orderitems and delete the order items also 
+            await order.orderItems.map(async orderItem=>{
+                await OrderItem.findByIdAndRemove(orderItem);
+            })
+            return res.status(200).json({
+                success: true,
+                message: 'the Order is deleted'
+            })
+        }else {
+            return res.status(400).json({
+                success: false,
+                message: 'Cannot find the Order'
+            })
+        }
+    }).catch(error=>{
+        return res.status(400).json({
+            success:false,
+            error
+        })
+    })
 }
--- routes/order/index.js
+++ routes/order/index.js
@@ -7,8 +7,13 @@ const orderController=require('../../controllers/order_controller');
 
 router.get('/allorders', orderController.getAllOrder);
 
+router.get('/:id', orderController.getOrderById);
 
 router.post('/add', orderController.postOrders);
+
+router.put('/status/:id', orderController.updateOrderStatus);
+
+router.delete('/:id', orderController.deleteOrder);
 
 
 module.exports=router;
